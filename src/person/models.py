ce674299d4b1812595b68be48b5a31599cfb5ed2
poli <pkurochkina@diasoft.ru>
2024-10-08 21:57:22 +0300

    Классы для таблиц. Пользователи, тренировки, записи

---
+++ src/person/models.py
@@ -0,0 +1,27 @@
+import uuid
+
+from sqlalchemy import Column, String, UUID, TIMESTAMP, Float, Integer
+from database import Base
+
+class Person(Base):
+    def __init__(self,
+                 person_id: uuid.UUID,
+                 person_name: str,
+                 person_surname: str,
+                 person_patronimic: str,
+                 person_age: int,
+                 person_phone: str):
+        self.person_id = person_id
+        self.person_name = person_name
+        self.person_surname = person_surname
+        self.person_patronimic = person_patronimic
+        self.person_age = person_age
+        self.person_phone = person_phone
+
+    __tablename__ = 'person'
+    person_id = Column(UUID, primary_key=True)
+    person_name = Column(String)
+    person_surname = Column(String)
+    person_patronimic = Column(String)
+    person_age = Column(Integer)
+    person_phone = Column(String)
---
+++ src/training/models.py
@@ -0,0 +1,31 @@
+import datetime
+import uuid
+
+from sqlalchemy import Column, String, UUID, TIMESTAMP, Float, Integer
+from database import Base
+
+class Training(Base):
+    def __init__(self,
+                 training_id: uuid.UUID,
+                 training_name: str,
+                 training_description: str,
+                 training_date: datetime.datetime,
+                 training_status: int,
+                 training_space: int,
+                 training_free_space: str):
+        self.training_id = training_id
+        self.training_name = training_name
+        self.training_description = training_description
+        self.training_date = training_date
+        self.training_status = training_status
+        self.training_space = training_space
+        self.training_free_space = training_free_space
+
+    __tablename__ = 'person'
+    training_id = Column(UUID, primary_key=True)
+    training_name = Column(String)
+    training_description = Column(String)
+    training_date = Column(TIMESTAMP)
+    training_status = Column(Integer)
+    training_space = Column(Integer)
+    training_free_space = Column(String)
