579d89e2daa2259e42c206cd178fc708315a9108
poli <pkurochkina@diasoft.ru>
2024-10-10 14:25:39 +0300

    Попытки исправления ошибки при миграции

--- src/database.py
+++ src/database.py
@@ -1,23 +1,25 @@
 from typing import AsyncGenerator, Callable
 
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, AsyncEngine
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from typing import AsyncGenerator, Callable, Tuple
+from sqlalchemy.ext.asyncio import AsyncEngine
 
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
-def get_db(host, port, db_name, user, password) -> tuple[str, AsyncEngine, sessionmaker, Callable[[], AsyncGenerator[AsyncSession, None]]]:
-    url = f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{db_name}"
-    engine = create_async_engine(url)
-    session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
-    async def get_session() -> AsyncGenerator[AsyncSession, None]:
-        async with session_maker() as session:
-            yield session
-            await session.commit()
-
-    return url, engine, session_maker, get_session
-
-DATABASE_URL, engine, async_session_maker, get_async_session = get_db(DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS)
-
-class Base(DeclarativeBase): pass
+def get_db(host: str, port: str, db_name: str, user: str, password: str) -> Tuple[str, AsyncEngine, async_sessionmaker, Callable[[], AsyncGenerator[AsyncSession, None]]]:
+    url = f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{db_name}" 
+    engine = create_async_engine(url) 
+    session_maker = async_sessionmaker(engine, expire_on_commit=False) 
+ 
+    async def get_session() -> AsyncGenerator[AsyncSession, None]: 
+        async with session_maker() as session: 
+            yield session 
+            await session.commit() 
+ 
+    return url, engine, session_maker, get_session 
+ 
+DATABASE_URL, engine, async_session_maker, get_async_session = get_db(DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS) 
+ 
+class Base(declarative_base()): 
+    pass
