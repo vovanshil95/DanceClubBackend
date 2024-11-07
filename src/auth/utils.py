32c9f8f34c8303d9d6670096975e35dfa7902711
vovanshil95 <vovanshil95@mail.ru>
2024-11-07 21:39:53 +0300

    svo

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -20,10 +20,8 @@ class AccessTokenPayload(BaseModel):
     super_user: bool
 
 
-async def check_user_agent(user_agent: str = Header(...)):
-    if user_agent is None:
-        raise HTTPException(status_code=400, detail='error: User-Agent required')
-    return user_agent
+async def check_user_agent():
+    return 'AndroidUser'
 
 
 def encrypt(string: str | bytes) -> bytes:
