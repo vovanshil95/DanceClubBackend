893fc9b04f9d4e8e0a9c4bbc5b49c19b91ba9c88
vovanshil95 <vovanshil95@mail.ru>
2024-11-08 13:10:22 +0300

    add logging

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.utils import get_openapi
 
@@ -6,9 +7,9 @@ from training.routes import router as training_router
 from training_sign.routers import router as sign_router
 from person.routers import router as person_router
 from auth.routers import router as auth_router
-
-
+from error_handlers import http_val_err_handler, http_all_err_handler
 from config import ORIGINS
+
 
 app = FastAPI(title='DanceClub API',
               description='here is backend')
@@ -25,6 +26,9 @@ app.include_router(training_router)
 app.include_router(sign_router)
 app.include_router(person_router)
 app.include_router(auth_router)
+
+app.add_exception_handler(RequestValidationError, http_val_err_handler)
+app.add_exception_handler(HTTPException, http_all_err_handler)
 
 
 def custom_openapi():
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,19 @@
 from pydantic import BaseModel
+import os
+import subprocess
+
 
 class BaseResponse(BaseModel):
     message: str='status success'
+
+
+def find_process_by_port(port=8000):
+    result = subprocess.run(['lsof', '-i', f':{8000}'], capture_output=True, text=True)
+    for line in result.stdout.split('\n')[1:]:
+        process_id = line.split()[1]
+        return int(process_id)
+    return None
+
+
+def kill_process(process_id):
+    os.kill(process_id, 9)
